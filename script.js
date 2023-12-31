8b1df9543b506cea1d7f3b47b96ff66eeda11b48
Chikezie Ilodigwe <dikee5200@gmail.com>
2023-12-31 09:45:49 +0100

    js updated

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //make reference to the icon and menu in the html
-const icon = document.getElementsByClassName(".menu-toggle");
+const myIcon = document.getElementsByClassName(".menu-toggle");
 const myMenu = document.getElementsByClassName(".menu");
 //add event listener
-icon.addEventListener("click", function () {
+myIcon.addEventListener("click", function () {
   //toggle the menu-open css class to either show or hide the menu
   myMenu.classList.toggle("menu-open");
 });
