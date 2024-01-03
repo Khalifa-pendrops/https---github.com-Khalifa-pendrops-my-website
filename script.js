25da688805aba228b76008111b268d13ec7ef8ab
Chikezie Ilodigwe <dikee5200@gmail.com>
2024-01-03 12:19:58 +0100

    js still work-in-origress

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Make reference to the icon and menu in the HTML
 const myIcon = document.querySelector(".menu-toggle");
 const myMenu = document.querySelector(".menu");
+const button = document.querySelector(".btn");
+  const myName = document.getElementById("name")
+  const email = document.getElementById("email")
+  const message = document.getElementById("message")
 
 // Add event listener
 myIcon.addEventListener("click", function () {
@@ -14,19 +18,31 @@ myIcon.addEventListener("click", function () {
 });
 
 //for contact page
-function onclick() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-  if (name == "" || email == "" || message == "") {
-    document.getElementById("error-message").innerHTML =
-      "Please fill out all filds.";
-    return false;
-    //   alert("Please check and complete all required filds.");
-    // } else if (email == "@gmail.com") {
-    //   alert("Please use the correct email format");
+button.addEventListener("click", function () {
+  if (myName == "" || email == "" || message == "") {
+    alert("Please check and complete all required filds.")
     return false;
   } else {
     return true;
   }
-}
+});
+
+
+
+
+// function onclick() {
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
+//   const message = document.getElementById("message").value;
+//   if (name == "" || email == "" || message == "") {
+//     // document.getElementById("error-message").innerHTML =
+//     //   "Please fill out all filds.";
+//     // return false;
+//       alert("Please check and complete all required filds.");
+//     } else if (email == "@gmail.com") {
+//       alert("Please use the correct email format");
+//     return false;
+//   } else {
+//     return true;
+//   }
+
